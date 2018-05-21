c95a7a0cbba66e1b5f8a6a7d0693ead938098455
Yanchuk Taras <YATV@users.noreply.github.com>
2018-05-21 00:53:41 -0300

    Update watchdog.sh
    Commented debug log added

--- root/watchdog.sh
+++ root/watchdog.sh
@@ -65,15 +65,20 @@ if [ "$INT2_IP" = "gateway" ] ; then
 	INT2_IP=`ip route | awk '/default/{print$3}'`
 fi
 
+#echo "`date '+%Y-%m-%d %H:%M:%S'` - [?/?/?] Watchdog started ( $timenow $watchdogdelay $delay $pingfailcountfile $pingfailcount)" >> $watchdoglogfile
 if [ "$[$timenow-$watchdogdelay]" -ge "$delay" ] ; then
+	#echo "`date '+%Y-%m-%d %H:%M:%S'` - [?/?/?] Start ping EXTERNAL IP ($EXT_IP)" >> $watchdoglogfile
 	sudo /bin/ping -c$pingcount $EXT_IP &> /dev/null && result="success" || result="fail"
 	if [ "$result" = "fail" ] ; then
+		#echo "`date '+%Y-%m-%d %H:%M:%S'` - [F/?/?] Start ping INTERNAL IP ($INT1_IP)" >> $watchdoglogfile
 		sudo /bin/ping -c$pingcount $INT1_IP &> /dev/null && result="success" || result="fail"
 		if [ "$result" = "fail" ] ; then
+			#echo "`date '+%Y-%m-%d %H:%M:%S'` - [F/F/?] Start ping INTERNAL IP ($INT2_IP)" >> $watchdoglogfile
 			sudo /bin/ping -c$pingcount $INT2_IP &> /dev/null && result="success" || result="fail"
 			if [ "$result" = "fail" ] ; then
 				let "pingfailcount += 1"
 				echo "$pingfailcount" > $pingfailcountfile
+				#echo "`date '+%Y-%m-%d %H:%M:%S'` - [F/F/F] pingfailcount:  $pingfailcount , maxpingfail: $maxpingfail" >> $watchdoglogfile
 				if [ "$pingfailcount" -ge "$maxpingfail" ] ; then
 					echo "`date '+%Y-%m-%d %H:%M:%S'` - [F/F/F] All pings failed! [Count: $pingfailcount] Reboot NOW!" >> $watchdoglogfile
 					/sbin/reboot
@@ -93,7 +98,7 @@ if [ "$[$timenow-$watchdogdelay]" -ge "$delay" ] ; then
 			fi
 		fi
 	else
-		echo "`date '+%Y-%m-%d %H:%M:%S'` - [T/?/?] All good! " >> $watchdoglogfile
+		#echo "`date '+%Y-%m-%d %H:%M:%S'` - [T/?/?] All good! " >> $watchdoglogfile
 		if [ "$pingfailcount" -ge "0" ] ; then
 			echo "0" > $pingfailcountfile
 		fi
